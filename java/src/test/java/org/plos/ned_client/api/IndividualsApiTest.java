ddd74bbe56db81dd18f7a1c9c17c45f44d1c3122
Jono <jono@foodnotblogs.com>
2016-03-17 21:08:55 -0700

    More getting ready of unit tests

--- java/src/test/java/org/plos/ned_client/api/IndividualsApiTest.java
+++ java/src/test/java/org/plos/ned_client/api/IndividualsApiTest.java
@@ -1,6 +1,8 @@
 package org.plos.ned_client.api;
 
-// import org.plos.ned_client.api.*;
+import org.plos.ned_client.*;
+import org.plos.ned_client.api.*;
+import org.plos.ned_client.model.*;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -14,7 +16,7 @@ import static org.junit.Assert.*;
 public class IndividualsApiTest {
 
   @Test
-  public void testIndividual() {
+  public void testCreateApi() {
 
     ApiClient client = new ApiClient();
     client.setBasePath("http://example.com");
@@ -22,22 +24,38 @@ public class IndividualsApiTest {
 
     IndividualsApi api = new IndividualsApi(client);
 
-
-    // TODO: finish from https://github.com/fehguy/swagger-codegen/blob/sample/java8/samples/client/petstore/java/default/src/test/java/io/swagger/petstore/test/PetApiTest.java
-
-    // Group g = new Group();
-    //
-    // g.setStartdate(LocalDate.of(1994, 2, 4));
-    //
-    // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    //
-    // g.setEnddate(LocalDate.parse("2005-01-12", formatter));
-    //
-    // assertEquals(g.getEnddate().toString(), "2005-01-12");
-    // assertEquals(g.getStartdate().toString(), "1994-02-04");
-
-    // g.setCreated = new Date(1994, 2, 6);
-
-    // assertEquals(g.getCreated().toString(), "1994-02-06");
+    assertNotNull(api.getApiClient());
   }
+
+  @Test
+  public void testCreateIndividual() throws ApiException {
+
+    IndividualsApi api = new IndividualsApi();
+
+    IndividualComposite c = new IndividualComposite();
+
+    Individualprofile p = new Individualprofile();
+    p.setId(1);
+    p.setFirstname("test");
+    List<Individualprofile> ps = new ArrayList<Individualprofile>();
+    ps.add(p);
+
+    c.setIndividualprofiles(ps);
+
+    Group g = new Group();
+    g.setId(1);
+    g.setStartdate(LocalDate.of(1996, 2, 4));
+
+    List<Group> gs = new ArrayList<Group>();
+    gs.add(g);
+
+    c.setGroups(gs);
+
+    // TODO: mock request to/from server
+    api.createIndividual(c, null);
+
+  }
+
+  // TODO: finish from https://github.com/fehguy/swagger-codegen/blob/sample/java8/samples/client/petstore/java/default/src/test/java/io/swagger/petstore/test/PetApiTest.java
+
 }
