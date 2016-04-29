a66ccac2733c9eee9589e6779b113f021664bf2b
Jono Finger <jfinger@plos.org>
2016-04-28 17:23:27 -0700

    PS-1526 Showing that client does not print credentials

--- sample-java/src/main/java/org/plos/ned/App.java
+++ sample-java/src/main/java/org/plos/ned/App.java
@@ -3,6 +3,7 @@ package org.plos.ned;
 import org.plos.ned_client.*;
 import org.plos.ned_client.api.*;
 import org.plos.ned_client.auth.*;
+import org.plos.ned_client.model.*;
 
 import java.io.UnsupportedEncodingException;
 import javax.xml.bind.DatatypeConverter;
@@ -11,7 +12,7 @@ public class App {
   public static void main( String[] args ) throws ApiException {
 
     ApiClient apiclient = new ApiClient();
-    apiclient.setBasePath("http://digitus.plos.org:8080/v1");
+    apiclient.setBasePath("http://localhost:8080/v1");
     apiclient.setUsername("akita");
     apiclient.setPassword("akita");
     apiclient.setDebugging(true);
@@ -25,9 +26,13 @@ public class App {
     System.out.println("typeclass description: " +
     typeclassesapi.read(12).getDescription());
 
-    individualsApi.readIndividual(476550);
+    IndividualComposite composite = individualsApi.readIndividual(1000000);
 
-    System.out.println("Start date: " +individualsApi.getGroups(476550).get(0).getStartdate());
+    System.out.println("emails: " + composite.getEmails().size());
+    System.out.println("auths: " + composite.getAuth().size());
+    System.out.println(composite);
+
+    // System.out.println("Start date: " +individualsApi.getGroups(476550).get(0).getStartdate());
 
   }
 }
